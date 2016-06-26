4b80e2b2f05587b075e20c9014bbde0e6a586593
Francesco <fr.ozzimo@gmail.com>
2016-06-26 16:09:03 +0200

    UI improvements: media type filter and progress circula while loading files

--- ui/controllers/galleryController.js
+++ ui/controllers/galleryController.js
@@ -3,6 +3,8 @@
     
     var galleryController = function($scope, GalleryService, $mdSidenav) {
         $scope.currentDir = "";
+        $scope.currentFilter = "";
+        $scope.loading = false;
 
         GalleryService.getAvailableGalleries()
         .then(function(res) {
@@ -18,7 +20,12 @@
             .then(function(res) {
                 $scope.files = res.data;
                 $scope.currentDir = dir;
+                $scope.loading = false;
             });
+            
+            $scope.files = null;
+            $scope.loading = true;
+            $mdSidenav('left').toggle();
         }
         
         $scope.open = function(image) {
