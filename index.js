a5428a9f961b0b617c3422592f98abf6b0d3b5b0
Torsten Werner <torsten.werner@westernacher.com>
2016-12-03 19:49:15 +0100

    make scripts executable

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var configuration = require('./config');
 var server = require('./app');
+var spawn = require('child_process').spawn;
 
 server.listen(configuration.port, function () {
-        console.log('Running on port '+ configuration.port);
+        const url = 'http://localhost:' + configuration.port;
+        console.log('Running on ' + url);
+        spawn('open', [url]);
 });
